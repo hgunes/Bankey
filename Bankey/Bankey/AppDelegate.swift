320a0d998bf3828089e04ddbf2b3216c3fbfe6a8
harungunes <h.gunes13@gmail.com>
2022-05-07 21:48:42 +0200

    add onboarding VC and page view controller

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.makeKeyAndVisible()
-    window?.rootViewController = LoginViewController()
+//    window?.rootViewController = LoginViewController()
+    window?.rootViewController = OnboardingContainerVC()
     
     return true
   }
