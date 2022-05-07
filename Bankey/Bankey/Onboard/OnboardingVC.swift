8f3940d59e992ad3f9a3c8e3cbfbc9aaf7b47ce8
harungunes <h.gunes13@gmail.com>
2022-05-07 22:37:05 +0200

    modify OnboardingVC

--- Bankey/Bankey/Onboard/OnboardingVC.swift
+++ Bankey/Bankey/Onboard/OnboardingVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class OnboardingVC: UIViewController {
   
   let stackView = UIStackView()
+  let imageView = UIImageView()
   let label = UILabel()
   
   
@@ -30,19 +31,30 @@ extension OnboardingVC {
     stackView.axis = .vertical
     stackView.spacing = 20
     
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleAspectFit
+    imageView.image = UIImage(named: "delorean")
+    
     label.translatesAutoresizingMaskIntoConstraints = false
-    label.text = "Welcome"
-    label.font = UIFont.preferredFont(forTextStyle: .title1)
+    label.font = UIFont.preferredFont(forTextStyle: .title2)
+    label.adjustsFontForContentSizeCategory = true
+    label.textAlignment = .center
+    label.numberOfLines = 0
+    label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel lika you are back in 1989."
   }
   
   
   func layout() {
+    stackView.addArrangedSubview(imageView)
     stackView.addArrangedSubview(label)
+    
     view.addSubview(stackView)
     
     NSLayoutConstraint.activate([
       stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+      view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
     ])
   }
 }
