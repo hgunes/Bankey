d48cef0f8dbfa18bd19e96908b434acd41bc7331
harungunes <h.gunes13@gmail.com>
2022-05-29 15:38:13 +0200

    refactor: general AppDelegate cleanup

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -11,75 +11,79 @@ let appColor: UIColor = .systemPurple
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    let loginViewController = LoginViewController()
+    let onboardingViewController = OnboardingContainerVC()
+    let mainViewController = MainViewController()
+        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+                
+        loginViewController.delegate = self
+        onboardingViewController.delegate = self
 
-  var window: UIWindow?
-  
-  let loginVC = LoginViewController()
-  let onboardingContainerVC = OnboardingContainerVC()
-  let mainVC = MainViewController()
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.makeKeyAndVisible()
-    
-    loginVC.delegate = self
-    onboardingContainerVC.delegate = self
+        displayLogin()
+        return true
+    }
 
-    let vc = mainVC
-    vc.setStatusBar()
-    
-    UINavigationBar.appearance().isTranslucent = false
-    UINavigationBar.appearance().backgroundColor = appColor
-    
-    window?.rootViewController = vc
-    
-    return true
-  }
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
 
+    private func displayNextScreen() {
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingViewController)
+        }
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
+    }
 }
 
 extension AppDelegate {
-  func setRootVC(_ vc: UIViewController, animated: Bool = true) {
-    guard animated, let window = self.window else {
-      self.window?.rootViewController = vc
-      self.window?.makeKeyAndVisible()
-      return
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
     }
-    
-    window.rootViewController = vc
-    window.makeKeyAndVisible()
-    UIView.transition(with: window,
-                      duration: 0.3,
-                      options: .transitionCrossDissolve,
-                      animations: nil,
-                      completion: nil)
-  }
 }
 
 extension AppDelegate: LoginViewControllerDelegate {
-  func didLogin() {
-    if LocalState.hasOnboarded  {
-      setRootVC(mainVC)
-    } else {
-      setRootVC(onboardingContainerVC)
+    func didLogin() {
+        displayNextScreen()
     }
-    
-  }
 }
 
 extension AppDelegate: OnboardingContainerVCDelegate {
-  func didFinishOnboarding() {
-    LocalState.hasOnboarded = true
-    setRootVC(mainVC)
-  }
-  
-  
+    func didFinishOnboarding() {
+        LocalState.hasOnboarded = true
+        prepMainView()
+        setRootViewController(mainViewController)
+    }
 }
 
 extension AppDelegate: LogoutDelegate {
-  func didLogout() {
-    setRootVC(loginVC)
-  }
+    func didLogout() {
+        setRootViewController(loginViewController)
+    }
 }
-
